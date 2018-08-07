2dd2f03d2efceaf1b056cc03239132fb700e124f
Karan Raina <karanraina1996@gmail.com>
2018-08-07 19:26:16 +0530

    sharda added

---
+++ js/myjs.js
@@ -0,0 +1,84 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCZhX24w5s0zoD9TR_hQag37eHOSWyT7RY",
+    authDomain: "sharda-web-tutorials.firebaseapp.com",
+    databaseURL: "https://sharda-web-tutorials.firebaseio.com",
+    projectId: "sharda-web-tutorials",
+    storageBucket: "",
+    messagingSenderId: "420632482729"
+  };
+  firebase.initializeApp(config);
+
+var db = firebase.database();
+
+function handleSignUp() {
+     var email = document.getElementById('email').value;
+      var password = document.getElementById('pwd').value;
+      var name = document.getElementById('name').value;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      // [START createwithemail]
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+      });
+      // [END createwithemail]
+      
+      db.ref().child('Users').child(firebase.auth().currentUser.uid).child('Name').set(name);
+      window.alert('Hath Hath Mubarak!!! Welcome!!!');
+      
+     window.open("login.html",'_self');
+    }
+
+ function toggleSignIn() {
+     // if (firebase.auth().currentUser) {
+        // [START signout]
+//firebase.auth().signOut();
+        // [END signout]
+     // } else {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (password.length < 4) {
+          alert('Please enter a password.');
+          return;
+        }
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authwithemail]
+        
+        window.location.href = "https://www.google.com";
+      }
