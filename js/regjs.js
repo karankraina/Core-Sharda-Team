9ec96f258ccb45d3ce12317571679e6b79a47f0c
Karan Raina <karanraina1996@gmail.com>
2018-08-07 20:25:51 +0530

    new

--- js/regjs.js
+++ js/regjs.js
@@ -41,13 +41,65 @@ function handleSignUp() {
       // [END createwithemail]
       
       db.ref().child('Users').child(firebase.auth().currentUser.uid).child('Name').set(name);
+      
+    }
+
+
+    function toggleSignIn() {
+     // if (firebase.auth().currentUser) {
+        // [START signout]
+//firebase.auth().signOut();
+        // [END signout]
+     // } else {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (password.length < 4) {
+          alert('Please enter a password.');
+          return;
+        }
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authwithemail]
+        window.alert('Welcome!!!');
+        window.open("index.html",'_self');
+    }
+      
+
+      firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+      console.log("Hurray ! signed in successfully !");
       window.alert('Hath Hath Mubarak!!! Welcome!!!');
       
      window.open("login.html",'_self');
-    }
+  } else {
+    // No user is signed in.
+     console.log("Sign in required !");
+     $("#login-btn-nav").click();
+  }
+});
+
 
 function gotolog(){
-  window.open("login.html",'_self');
+  window.open("register.html",'_self');
     
 
 }
